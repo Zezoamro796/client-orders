6d93a7b874abf39e7716373daaa83e88af947a5a
Zezoamro796 <67561322+Zezoamro796@users.noreply.github.com>
2025-05-21 13:59:16 +0300

    Delete get_orders.php

--- get_orders.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db.php";
-$res = $conn->query("SELECT * FROM orders ORDER BY id DESC");
-$orders = [];
-while ($row = $res->fetch_assoc()) {
-    $orders[] = $row;
-}
-echo json_encode($orders);
-?>
